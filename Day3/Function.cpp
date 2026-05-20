026d95b7062ada47f89d8a284f3ee32e17f4dbe2
Atul <adadwal942@gmail.com>
2026-05-20 11:02:38 +0530

    functions

--- Day3/Function.cpp
+++ Day3/Function.cpp
@@ -7,3 +7,45 @@ void greet (){
 int main(){
   greet();
 }
+
+//Types of Function 
+
+//Function without parameter
+
+#include<iostream>
+using namespace std;
+
+void name(){
+  cout<<"My Name is Atul Dadwal";
+}
+int main(){
+  name();
+}
+
+//Function with parameter
+
+#include<iostream>
+using namespace std;
+
+void sum(int a , int b){
+  cout<<"Sum"<<" "<<a+b;
+}
+
+int main(){
+  sum(4,7);
+}
+
+//Function with return value
+
+#include<iostream>
+using namespace std;
+
+int sums(int a , int b){
+  int sums = a+b;
+  return sums;
+}
+int main(){
+  int result;
+  result = sums(5,3);
+  cout<<"Sum "<<result<<endl;
+}
