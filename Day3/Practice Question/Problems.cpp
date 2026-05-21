551a24ff7249c8215b18ce229ae9233f975b6a13
Atul <adadwal942@gmail.com>
2026-05-21 21:26:22 +0530

    building logic building and enchance code writing skill

--- Day3/Practice Question/Problems.cpp
+++ Day3/Practice Question/Problems.cpp
@@ -65,3 +65,99 @@
 //   cout<<count(n);
 // }
 
+
+//Reverse digit
+
+// #include<iostream>
+// using namespace std;
+// int reverse(int n ){
+//   int rev = 0;
+//   while(n>0){
+//     int digit = n%10;
+//     rev = rev *10 +digit;
+//     n = n/10;
+//   }
+//   return rev;
+  
+// }
+// int main(){
+//   int n;
+//   cin>>n;
+//   cout<<reverse(n)<<endl;
+// }
+
+//Palidrome Number
+
+// #include<iostream>
+// using namespace std;
+
+// int reverse(int n){
+//   int rev = 0;
+//   while(n>0){
+//     int digit = n%10;
+//     rev = rev*10+digit;
+//     n = n/10;
+//   }
+//   return rev;
+
+// }
+
+// int palidrome(int n){
+//     if(reverse(n)== n){
+//     cout<<"This is palidrome number"<<endl;
+//   }else{
+//     cout<<"This is not a palidrome number";
+//   }
+//   return true;
+// }
+
+// int main(){
+//   int n;
+//   cin>>n;
+//   cout<<palidrome(n)<<endl;
+// }
+
+//Sum of Digits
+
+// #include<iostream>
+// using namespace std;
+
+// int sum(int n){
+//   int rev = 0;
+//   int sums = 0;
+
+//   while(n>0){
+//     int digit = n%10;
+//     rev = rev * 10 + digit;
+//     n = n/10;
+//     sums = sums +digit;
+//   }
+//   return sums;
+// }
+// int main(){
+//   int n;
+//   cin>>n;
+
+//   cout<<sum(n)<<endl;
+// }
+
+//Armstrong number
+
+#include<iostream>
+using namespace std;
+int Armstrong(int n){
+  int original = n;
+  int arm = 0;
+  while(n>0){
+    int digit = n%10;
+    arm = arm + digit*digit*digit;
+    n =n /10;
+  }
+  return arm;
+}
+int main(){
+  int n;
+  cin>>n;
+
+  cout<<Armstrong(n)<<endl;
+}
