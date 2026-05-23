6877da53d3f52dccd4c7af00619ebb2c66852a98
Atul <adadwal942@gmail.com>
2026-05-23 16:33:48 +0530

    learning and solving more array problem

--- Day3/Practice Question/Problems.cpp
+++ Day3/Practice Question/Problems.cpp
@@ -1,145 +1,145 @@
 // Factorial
 
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int factorial(int n){
-//   int fact = 1;
-//   for(int i = 1; i<=n;i++){
-//     fact = fact*i;
-//   }
-//   return fact;
-// }
-// int main(){
-//   int n;
-//   cin>>n;
-//   factorial(n);
-// }
+int factorial(int n){
+  int fact = 1;
+  for(int i = 1; i<=n;i++){
+    fact = fact*i;
+  }
+  return fact;
+}
+int main(){
+  int n;
+  cin>>n;
+  factorial(n);
+}
 
 //Prime number
 
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int prime(int n){
-//   if(n<=1){
-//     return false;
-//   };
-//   for(int i = 2;i<n;i++){
-//     if(n%i==0){
-//       return false;
-//     }
-//   }
-//   return true;
-// }
+int prime(int n){
+  if(n<=1){
+    return false;
+  };
+  for(int i = 2;i<n;i++){
+    if(n%i==0){
+      return false;
+    }
+  }
+  return true;
+}
 
-// int main(){
-//   int n;
-//   cin>>n;
+int main(){
+  int n;
+  cin>>n;
 
-//   if(prime(n)){
-//     cout<<"It is Prime Number";
-//   } else{
-//     cout<<"It is not a Prime Number";
-//   };
-// }
+  if(prime(n)){
+    cout<<"It is Prime Number";
+  } else{
+    cout<<"It is not a Prime Number";
+  };
+}
 
 // Count Digits
 
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int count(int n){
-//   int count = 0;
-//   for(int i = 1; i<n;i++){
-//     count++;
-//     n = n/10;
-//   }
-//   return count;
-// }
+int count(int n){
+  int count = 0;
+  for(int i = 1; i<n;i++){
+    count++;
+    n = n/10;
+  }
+  return count;
+}
 
-// int main(){
-//   int n;
-//   cin>>n;
+int main(){
+  int n;
+  cin>>n;
 
-//   cout<<count(n);
-// }
+  cout<<count(n);
+}
 
 
 //Reverse digit
 
-// #include<iostream>
-// using namespace std;
-// int reverse(int n ){
-//   int rev = 0;
-//   while(n>0){
-//     int digit = n%10;
-//     rev = rev *10 +digit;
-//     n = n/10;
-//   }
-//   return rev;
+#include<iostream>
+using namespace std;
+int reverse(int n ){
+  int rev = 0;
+  while(n>0){
+    int digit = n%10;
+    rev = rev *10 +digit;
+    n = n/10;
+  }
+  return rev;
   
-// }
-// int main(){
-//   int n;
-//   cin>>n;
-//   cout<<reverse(n)<<endl;
-// }
+}
+int main(){
+  int n;
+  cin>>n;
+  cout<<reverse(n)<<endl;
+}
 
 //Palidrome Number
 
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int reverse(int n){
-//   int rev = 0;
-//   while(n>0){
-//     int digit = n%10;
-//     rev = rev*10+digit;
-//     n = n/10;
-//   }
-//   return rev;
+int reverse(int n){
+  int rev = 0;
+  while(n>0){
+    int digit = n%10;
+    rev = rev*10+digit;
+    n = n/10;
+  }
+  return rev;
 
-// }
+}
 
-// int palidrome(int n){
-//     if(reverse(n)== n){
-//     cout<<"This is palidrome number"<<endl;
-//   }else{
-//     cout<<"This is not a palidrome number";
-//   }
-//   return true;
-// }
+int palidrome(int n){
+    if(reverse(n)== n){
+    cout<<"This is palidrome number"<<endl;
+  }else{
+    cout<<"This is not a palidrome number";
+  }
+  return true;
+}
 
-// int main(){
-//   int n;
-//   cin>>n;
-//   cout<<palidrome(n)<<endl;
-// }
+int main(){
+  int n;
+  cin>>n;
+  cout<<palidrome(n)<<endl;
+}
 
 //Sum of Digits
 
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int sum(int n){
-//   int rev = 0;
-//   int sums = 0;
+int sum(int n){
+  int rev = 0;
+  int sums = 0;
 
-//   while(n>0){
-//     int digit = n%10;
-//     rev = rev * 10 + digit;
-//     n = n/10;
-//     sums = sums +digit;
-//   }
-//   return sums;
-// }
-// int main(){
-//   int n;
-//   cin>>n;
+  while(n>0){
+    int digit = n%10;
+    rev = rev * 10 + digit;
+    n = n/10;
+    sums = sums +digit;
+  }
+  return sums;
+}
+int main(){
+  int n;
+  cin>>n;
 
-//   cout<<sum(n)<<endl;
-// }
+  cout<<sum(n)<<endl;
+}
 
 //Armstrong number
 
--- Day4/Array.cpp
+++ Day4/Array.cpp
@@ -1,166 +1,166 @@
 // Input and Output of Array
 
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// void arra(){
-//   int n;
-//   cin>>n;
-//   int arr[n];
-//   for(int i = 0 ;i<n;i++){
-//     cin>>arr[i];
-//   }
+void arra(){
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i = 0 ;i<n;i++){
+    cin>>arr[i];
+  }
 
-//   for(int i = 0 ; i<n;i++){
-//     cout<<arr[i]<<" ";
-//   }
-// }
+  for(int i = 0 ; i<n;i++){
+    cout<<arr[i]<<" ";
+  }
+}
 
-// int main(){
+int main(){
 
-//   arra();
-// }
+  arra();
+}
 
 // Maximun Element in array
 
-// #include<iostream>
-// using namespace std;
-// int maxi(){
-//   int n ;
-//   cin>>n;
-//   int arr[5];
+#include<iostream>
+using namespace std;
+int maxi(){
+  int n ;
+  cin>>n;
+  int arr[5];
 
-//   for(int i = 0; i<n;i++){
-//     cin>>arr[i];
-//   }
+  for(int i = 0; i<n;i++){
+    cin>>arr[i];
+  }
 
-//   int max = arr[0];
-//   for(int i = 0; i<n;i++){
+  int max = arr[0];
+  for(int i = 0; i<n;i++){
     
-//     if(max<arr[i]){
-//       max = arr[i];
-//     }
-//   }
-//   cout<<max;
-//   return max;
+    if(max<arr[i]){
+      max = arr[i];
+    }
+  }
+  cout<<max;
+  return max;
   
-// }
+}
 
-// int main(){
-//   maxi();
-// }
+int main(){
+  maxi();
+}
 
-//Minimum Element in array
+// Minimum Element in array
 
-// #include<iostream>
-// using namespace std;
-// int mini(){
-//   int n;
-//   cin>>n;
-//   int arr[n];
-//   for(int i = 0; i<n;i++){
-//     cin>>arr[i];
-//   }
+#include<iostream>
+using namespace std;
+int mini(){
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i = 0; i<n;i++){
+    cin>>arr[i];
+  }
   
-//   int min = arr[0];
-//   for(int i = 0;i<n;i++){
-//     if(min>arr[i]){
-//       min = arr[i];
-//     }
-//   }
-//   cout<<min;
-//   return min;
+  int min = arr[0];
+  for(int i = 0;i<n;i++){
+    if(min>arr[i]){
+      min = arr[i];
+    }
+  }
+  cout<<min;
+  return min;
 
-// } 
-// int main(){
-//   mini();
-// }
+} 
+int main(){
+  mini();
+}
 
-//Sum of Array Element
+// Sum of Array Element
 
-// #include<iostream>
-// using namespace std;
-// int sum(){
-//   int n;
-//   cin>>n;
-//   int arr[n];
-//   for(int i = 0;i<n;i++){
-//     cin>>arr[i];
-//   }
-//   int add = 0;
-//   for(int i = 0; i<n;i++){
-//     add = add + arr[i];
-//   }
-//   cout<<add;
-//   return add;
-// }
-// int main(){
-//   sum();
-// }
+#include<iostream>
+using namespace std;
+int sum(){
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i = 0;i<n;i++){
+    cin>>arr[i];
+  }
+  int add = 0;
+  for(int i = 0; i<n;i++){
+    add = add + arr[i];
+  }
+  cout<<add;
+  return add;
+}
+int main(){
+  sum();
+}
 
-//Count Even and Odd Numbers
+// Count Even and Odd Numbers
 
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int evenodd(){
-//   int n;
-//   cin>>n;
+int evenodd(){
+  int n;
+  cin>>n;
 
-//   int arr[n];
-//   for(int i = 0; i<n;i++){
-//     cin>>arr[i];
-//   }
-//   int even = 0;
-//   int odd = 0;
-//   for(int i = 0; i<n;i++){
-//     if(arr[i]%2==0){
-//       even++;
-//     }else{
-//       odd++;
-//     }
-//   }
-//   cout<<"Even: "<<even<<" "<<"Odd: "<<odd<<endl;
-//   return even, odd;
-// }
+  int arr[n];
+  for(int i = 0; i<n;i++){
+    cin>>arr[i];
+  }
+  int even = 0;
+  int odd = 0;
+  for(int i = 0; i<n;i++){
+    if(arr[i]%2==0){
+      even++;
+    }else{
+      odd++;
+    }
+  }
+  cout<<"Even: "<<even<<" "<<"Odd: "<<odd<<endl;
+  return even, odd;
+}
 
-// int main(){
-//   evenodd();
-// }
+int main(){
+  evenodd();
+}
 
-//Linear Search
+// Linear Search
 
-// #include<iostream>
-// using namespace std;
-// int linear(){
-//   int n;
-//   cin>>n;
+#include<iostream>
+using namespace std;
+int linear(){
+  int n;
+  cin>>n;
   
-//   int arr[n];
-//   for(int i = 0 ;i<n;i++){
-//     cin>>arr[i];
-//   }
+  int arr[n];
+  for(int i = 0 ;i<n;i++){
+    cin>>arr[i];
+  }
 
-//   int target;
-//   cin>>target;
-//   int find = false;
-//   for(int i = 0; i<n;i++){
-//     if(target==arr[i]){
-//       find = true;
-//       break;
-//     }
-//   }
-//   if(find){
-//     cout<<"Founded";
-//   }else{
-//     cout<<"Not Founded";
-//   }
-// }
-// int main(){
-//   linear();
-// }
+  int target;
+  cin>>target;
+  int find = false;
+  for(int i = 0; i<n;i++){
+    if(target==arr[i]){
+      find = true;
+      break;
+    }
+  }
+  if(find){
+    cout<<"Founded";
+  }else{
+    cout<<"Not Founded";
+  }
+}
+int main(){
+  linear();
+}
 
-//Reverse an array
+// Reverse an array
 
 #include<iostream>
 using namespace std;
@@ -192,4 +192,98 @@ int reverse(){
 }
 int main(){
   reverse();
+}
+
+// Find Second Largest Element in Array
+
+#include<iostream>
+using namespace std;
+int second(){
+  int n;
+  cin>>n;
+  int arr[n];
+
+  for(int i =0;i<n;i++){
+    cin>>arr[i];
+  }
+  int largest = 0;
+  int secondlargest = 0;
+  for(int i = 0; i< n; i++){
+    if(largest<arr[i]){
+      secondlargest = largest;
+      largest = arr[i];
+    } else if(arr[i]>secondlargest && arr[i]!=largest){
+      secondlargest = arr[i];
+    };
+  }
+  cout<<largest;
+
+  cout<<secondlargest;
+}
+int main(){
+second();
+}
+
+// Find positve and negative element in array
+
+#include<iostream>
+using namespace std;
+int element(){
+  int n;
+  cin>>n;
+
+  int arr[n];
+  for(int i = 0;i<n;i++){
+    cin>>arr[i];
+  }
+
+  int positive = 0;
+  int negative = 0;
+  int zero = 0;
+  for(int i = 0; i<n;i++){
+    if(arr[i]>0){
+      positive++;
+    }else if(arr[i]<0){
+      negative++;
+    } else{
+      zero++;
+    }
+  }
+  cout<<"Positive Digit: "<<positive <<endl;
+  cout<<"Negative Digit: "<<negative <<endl;
+  cout<<"Zero Digit: "<<zero <<endl;
+}
+int main(){
+  element();
+}
+
+//Check array are sorted or not
+
+#include<iostream>
+using namespace std;
+int sorted(){
+  int n; 
+  cin>>n;
+
+  int arr[n];
+  for(int i = 0; i<n;i++){
+    cin>>arr[i];
+  }
+  bool sort = true;
+  for(int i =0 ;i<n;i++){
+    if(arr[i]>arr[i+1]){
+      sort = false;
+      break;
+    }
+    } 
+    if(sort ) {
+      cout<<"Sorted";
+    }else {
+      cout<<"Unsorted";
+    }
+    
+  }
+
+int main(){
+  sorted();
 }
